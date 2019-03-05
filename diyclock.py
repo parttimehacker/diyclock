393c549df9adaeed7a944892edfacf2537c495fc
Dave Wilson <parttimehacker@gmail.com>
2019-03-05 10:35:40 -0800

    First use of PyCharm

--- diyclock.py
+++ diyclock.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' Diyhas clock, motion detector and piezo alarm '''
+""" Diyhas clock, motion detector and piezo alarm """
 
 import time
 import datetime
@@ -16,35 +16,35 @@ from ledclock import *
 from ledmatrix8x8 import *
 
 class MqttTopicConfiguration:
-    ''' motion_topic to avoid global PEP8 '''
+    """ motion_topic to avoid global PEP8 """
     def __init__(self):
-        ''' create two topics for this application '''
+        """ create two topics for this application """
         self.setup_topic = "diyhas/" + socket.gethostname() + "/setup"
         self.motion_topic = ""
     def set(self, topic):
-        ''' the motion topic is passed to the app at startup '''
+        """ the motion topic is passed to the app at startup """
         print("set motion_topic=", topic)
         self.motion_topic = topic
     def get_setup(self,):
-        ''' the motion topic dynamically set '''
+        """ the motion topic dynamically set """
         return self.setup_topic
     def get_motion(self,):
-        ''' the motion topic dynamically set '''
+        """ the motion topic dynamically set """
         return self.motion_topic
 
 MOTION_TOPIC = MqttTopicConfiguration()
 
 class MotionController:
-    ''' motion detection device driver '''
+    """ motion detection device driver """
     def __init__(self, pin):
-        ''' capture interrupts on pin 17  '''
+        """ capture interrupts on pin 17  """
         self.gpio = GPIO.get_platform_gpio()
         self.queue = queue.Queue()
         self.pin = pin
         self.gpio.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     def pir_interrupt_handler(self, gpio):
-        ''' motion interrupt handler sends 1 or 0 to queue '''
+        """ motion interrupt handler sends 1 or 0 to queue """
         state = self.gpio.input(gpio)
         if state == 1:
             message = "1"
@@ -53,25 +53,25 @@ class MotionController:
         self.queue.put(message)
 
     def enable(self,):
-        ''' enable the interrupt handler '''
+        """ enable the interrupt handler """
         self.gpio.add_event_detect(self.pin, GPIO.RISING, callback=self.pir_interrupt_handler)
 
     def detected(self,):
-        ''' has motion been detected '''
+        """ has motion been detected """
         return not self.queue.empty()
 
     def get_motion(self,):
-        ''' return the last value either 1 or 0 '''
+        """ return the last value either 1 or 0 """
         return self.queue.get(False)
 
     def wait_for_motion(self,):
-        ''' wait for the next interrupt 1 or 0 '''
+        """ wait for the next interrupt 1 or 0 """
         return self.queue.get(True)
 
 class AlarmController:
-    ''' alarm piezo device driver '''
+    """ alarm piezo device driver """
     def __init__(self, pin):
-        ''' control output to pin 4  '''
+        """ control output to pin 4  """
         self.gpio = GPIO.get_platform_gpio()
         self.pin = pin
         self.gpio.setup(self.pin, GPIO.OUT)
@@ -97,10 +97,10 @@ ALARM.sound_alarm(False)
 
 
 class TimedEvents:
-    ''' timed event handler '''
+    """ timed event handler """
 
     def __init__(self, day, night):
-        ''' initialize night,day and light status '''
+        """ initialize night,day and light status """
         if night < day:
             raise "NIGHT < DAY"
         self.night = night
@@ -108,7 +108,7 @@ class TimedEvents:
         self.lights_are_on = True
 
     def control_lights(self,switch):
-        ''' dim lights at night or turn up during the day '''
+        """ dim lights at night or turn up during the day """
         if switch == "Turn On":
             CLOCK.set_brightness(15)
             MATRIX.set_mode(FIBINACCI_MODE)
@@ -119,7 +119,7 @@ class TimedEvents:
             self.lights_are_on = False
 
     def check_for_timed_events(self,):
-        ''' turn down displays at night '''
+        """ turn down displays at night """
         now = datetime.datetime.now().time()
         if now < self.day:
             if self.lights_are_on:
@@ -136,7 +136,7 @@ NIGHT_DEFAULT = datetime.time(21,1)
 TIMER = TimedEvents(DAY_DEFAULT,NIGHT_DEFAULT)
 
 def system_message(msg):
-    ''' process system messages'''
+    """ process system messages"""
     print("msg.payload=", msg.payload)
     if msg.topic == 'diyhas/system/fire':
         if msg.payload == b'ON':
@@ -184,8 +184,8 @@ TOPIC_DISPATCH_DICTIONARY = {
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    ''' Subscribing in on_connect() means that if we lose the connection and
-        reconnect then subscriptions will be renewed. '''
+    """ Subscribing in on_connect() means that if we lose the connection and
+        reconnect then subscriptions will be renewed. """
     client.subscribe("diyhas/system/fire", 1)
     client.subscribe("diyhas/system/panic", 1)
     client.subscribe("diyhas/system/who", 1)
@@ -194,7 +194,7 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    ''' dispatch to the appropriate MQTT topic handler '''
+    """ dispatch to the appropriate MQTT topic handler """
     TOPIC_DISPATCH_DICTIONARY[msg.topic]["method"](msg)
 
 MOTION = MotionController(17)
