f1402f2a5f75ac58a026dabaeab290f7145b26d4
Dave Wilson <parttimehacker@gmail.com>
2019-11-08 06:26:18 -0800

    Delete diyclockservice.sh
    Changing install stuff

--- diyclockservice.sh
+++
@@ -1,5 +0,0 @@
-sudo cp diyclock.service /lib/systemd/system/diyclock.service
-sudo chmod 644 /lib/systemd/system/diyclock.service
-sudo systemctl daemon-reload
-sudo systemctl enable diyclock.service
-
